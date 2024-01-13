df582fb78fa66aa16b165a276ef3456817da17b9
Jaideep Bose <jaideepbose51@gmail.com>
2024-01-13 10:58:14 +0530

    Added login and singup page in frontend and a strong passward checker in server

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -18,16 +18,56 @@ export const authentication = (req, res, next) => {
   });
 };
 
+//Strong password ckecker
+function isStrongPassword(password) {
+  // Minimum length requirement
+  const minLength = 8;
+
+  // Check for minimum length
+  if (password.length < minLength) {
+    return false;
+  }
+
+  // Check for at least one uppercase letter
+  if (!/[A-Z]/.test(password)) {
+    return false;
+  }
+
+  // Check for at least one lowercase letter
+  if (!/[a-z]/.test(password)) {
+    return false;
+  }
+
+  // Check for at least one digit
+  if (!/\d/.test(password)) {
+    return false;
+  }
+
+  // Check for at least one special character
+  if (!/[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(password)) {
+    return false;
+  }
+
+  // If all checks pass, the password is strong
+  return true;
+}
+
 export const createUser = async (req, res) => {
-  const user = { email: req.body.email, pass: req.body.password };
-  console.log(user);
-  try {
-    const newUser = new User(user);
-    await newUser.save();
-    console.log("OK");
-    res.status(201).redirect("/login");
-  } catch (error) {
-    res.status(409).json(error);
+  if (isStrongPassword(req.body.password)) {
+    const user = { email: req.body.email, pass: req.body.password };
+    console.log(user);
+    try {
+      const newUser = new User(user);
+      await newUser.save();
+      console.log("OK");
+      res.status(201).redirect("/login");
+    } catch (error) {
+      res.status(409).json(error);
+    }
+  } else {
+    console.log(
+      "Select strong passward. The password should contain atleast one uppercase , one lowercase , one digit , one special character and minimum length should be 8"
+    );
   }
 };
 
